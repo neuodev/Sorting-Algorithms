81e3213d1d94e45317d6db4960bc676f0fefd152
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-14 20:02:05 +0200

    Counting Sort

--- Counting_Sort.ts
+++ Counting_Sort.ts
@@ -1,11 +1,20 @@
 function countingSort(array: number[]) {
   let counting = [];
   for (let i = 0; i < array.length; i++) {
-    if (counting[i]) counting[i]++;
-    else counting[i] = 1;
+    let num = array[i];
+    if (counting[num] >= 0) counting[num]++;
+    else counting[num] = 1;
+  }
+  let start = 0;
+  for (let i = 0; i < counting.length; i++) {
+    if (counting[i] > 0) {
+      for (let j = 0; j < counting[i]; j++) {
+        array[start] = i;
+        start++;
+      }
+    }
   }
 
-  console.log(counting);
 }
 
-countingSort([5, 4, 2, 5, 4, 4, 5]);
+countingSort([5, 3, 2, 5, 4, 4, 5]);
