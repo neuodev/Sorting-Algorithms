a9006dbe1ba4030cbd2298d641956791945ecdca
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-14 16:57:43 +0200

    Apply Insertion  Sort algorithm

---
+++ Insertion_sort.ts
@@ -0,0 +1,23 @@
+insertionSort([9, 8, 7, 1, 4]);
+
+function insertionSort(array: number[]) {
+  for (let i = 0; i <= array.length; i++) {
+    sortSubArray(array, i);
+  }
+
+  console.log(array);
+}
+
+function sortSubArray(array: number[], i: number) {
+  let idx = i - 1;
+  while (idx >= 0) {
+    if (array[idx] < array[idx - 1]) {
+      swap(idx, idx - 1, array);
+    }
+    idx--;
+  }
+}
+
+function swap(j: number, i: number, array: number[]) {
+  [array[j], array[i]] = [array[i], array[j]];
+}
